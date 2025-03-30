68bba57e6304f116766ce4208abdcb54e7f4c7f8
Beckyyjw <beckyy.jw@gmail.com>
2025-03-30 12:18:09 +0200

    Updates

--- Pile.java
+++ Pile.java
@@ -1,3 +1,32 @@
 public class Pile {
-    //
+    private int[] elements;
+    private int nb;
+
+    public Pile(int max){
+        this.elements = new int[max];
+        this.nb = 0;
+    }
+
+    public void empiler(int element){
+        if(nb < elements.length){
+            elements[nb] = element;
+            nb ++;
+        }else {
+            System.out.println("La pile est pleine ! Impossible d'ajouter un élément.");
+        }
+    }
+
+    public int deplier(){
+        if(nb > 0){
+            nb --;
+            return elements[nb];
+        }
+        return -1;
+    }
+
+    public boolean estVide() {
+        return nb == 0; // Retourne true si la pile est vide (nb == 0), sinon false
+    }
+
+
 }
